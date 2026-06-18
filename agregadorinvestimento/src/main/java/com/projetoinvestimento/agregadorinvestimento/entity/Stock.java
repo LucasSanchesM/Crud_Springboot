97fc61ff7f87f8a748158b45652abb3e4bd7fbd9
Lucas Sanches Marcilio Pereira <luucaassanches@gmail.com>
2026-06-18 19:40:56 -0300

    Criada entidade estoque

---
+++ agregadorinvestimento/src/main/java/com/projetoinvestimento/agregadorinvestimento/entity/Stock.java
@@ -0,0 +1,44 @@
+package com.projetoinvestimento.agregadorinvestimento.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "stock")
+public class Stock {
+    @Id
+    @Column(name = "stock_id")
+    private String stock_id;
+    
+    @Column(name = "description")
+    private String description;
+
+    public Stock() {
+    }
+
+    public Stock(String stock_id, String description) {
+        this.stock_id = stock_id;
+        this.description = description;
+    }
+
+    public String getStock_id() {
+        return stock_id;
+    }
+
+    public void setStock_id(String stock_id) {
+        this.stock_id = stock_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+
+}
